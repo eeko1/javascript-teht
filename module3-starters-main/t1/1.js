41c216a41edf74bfc59061c7736622288dadad86
eeko1 <eemi.korhonen@metropolia.fi>
2022-11-28 16:41:06 +0200

    commit valmis

--- module3-starters-main/t1/1.js
+++ module3-starters-main/t1/1.js
@@ -1,23 +1,17 @@
 'use strict';
 let target = document.querySelector('#target')
-let item_amount = parseInt(prompt('Give number of items'))
 let items = []
 
-
-// Luetaan nimet
-for(let i = 0; i<item_amount; i++){
+for(let i = 0; i<3; i++){
     items[i] = prompt('Item: ');
 }
 
-
-// Generoidaan html-lista
 let html = '<ul class="my-list">';
 html += '<ol>';
-for(let i = 0; i< item_amount; i++){
+for(let i = 0; i<3; i++){
     html += '<li>';
     html += items[i];
     html += '</li>';
 }
-
 html += '</ol></ul>';
 target.innerHTML = html
